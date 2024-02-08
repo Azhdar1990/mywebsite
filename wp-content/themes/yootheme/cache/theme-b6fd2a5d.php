647ecb03831a7ca48b48361bdf0fed53b980253d
Azhdar Maniyev <azhdar@azdevops.net>
2024-02-08 11:16:10 +0400

    new theme

---
+++ wp-content/themes/yootheme/cache/theme-b6fd2a5d.php
@@ -0,0 +1,9 @@
+<?php // $file = C:\xampp\htdocs\wordpress\wp-content\themes\yootheme\packages\styler/config/theme.json
+
+return [
+  'defaults' => [
+    'less' => [], 
+    'custom_less' => '', 
+    'style' => 'fuse'
+  ]
+];
